3c8aca69fea179b4d8662ce31e4f4565523b2c68
lauraoliveri <lauraoliveri2003@gmail.com>
2024-09-01 19:36:38 +0200

    Complete Milestone 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,23 @@
 
   createApp({
     data() {
-      return {
-        Todolist: [
-            {task:"Comprare pane e latte", done: false},
-            {task: "Andare a ritirare il pacco Amazon", done: true},
-            {task: "Rispondere alle email di lavoro", done: true},
-            {task:"Completare esercizio di Boolean", done: true},
-            {task: "Fare skincare e maschera viso", done: false},
-            {task: "Mettere nuovi articoli su Vinted", done: false}
-        ]
+        return {
+            Todolist: [
+                {task:"Comprare pane e latte", done: false},
+                {task: "Andare a ritirare il pacco Amazon", done: true},
+                {task: "Rispondere alle email di lavoro", done: true},
+                {task:"Completare esercizio di Boolean", done: true},
+                {task: "Fare skincare e maschera viso", done: false},
+                {task: "Mettere nuovi articoli su Vinted", done: false}
+            ]
 
-      } 
+        }
+     
+    },
+    methods: {
+        RemoveTask (i) {
+            this.Todolist.splice(i, 1)
+        }
     }
   }).mount('#app')
 

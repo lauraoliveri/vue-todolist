3d4069b5759d0556f954826a5ca44690b981ab07
lauraoliveri <lauraoliveri2003@gmail.com>
2024-09-01 18:46:31 +0200

    Complete Milestone 1

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        Todolist: [
+            {task:"Comprare pane e latte", done: false},
+            {task: "Andare a ritirare il pacco Amazon", done: true},
+            {task: "Rispondere alle email di lavoro", done: true},
+            {task:"Completare esercizio di Boolean", done: true},
+            {task: "Fare skincare e maschera viso", done: false},
+            {task: "Mettere nuovi articoli su Vinted", done: false}
+        ]
+
+      } 
+    }
+  }).mount('#app')
+
